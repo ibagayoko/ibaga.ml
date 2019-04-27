e181f9d31e9989b45850f83bfb8476ec9013773e
Ismail bagayoko <bagayoko.ismail@gmail.com>
2019-04-27 13:32:23 -0300

    add avatar comp

---
+++ ibaga/resources/js/components/Avatar/Avatar.vue
@@ -0,0 +1,63 @@
+<script type="text/ecmascript">
+// Todo bind event function
+import { Icon } from "../";
+    export default  {
+        components: {
+            Icon
+        },
+        name: 'Avatar',
+        props: [
+            "className",
+            "children",
+            "imageURL",
+            "style",
+            "size",
+            "status",
+            "placeholder",
+            "icon",
+            "color",
+            "onClick",
+            "onMouseEnter",
+            "onMouseLeave",
+            "onPointerEnter",
+            "onPointerLeave",
+        ],
+        data() {
+            return {
+                styleObject: this.imageURL
+          ? Object.assign(
+              {
+                backgroundImage: `url(${this.imageURL})`,
+              },
+              this.style
+            )
+          : this.style
+                
+            }
+        },
+        mounted() {
+        },
+        methods: {},
+        computed: {
+      classObject: function () {
+        return {
+          avatar: true,
+      [`avatar-${this.size}`]: !!this.size,
+      "avatar-placeholder": this.placeholder,
+      [`avatar-${this.color}`]: !!this.color,
+            [`${this.className}`]:true
+        }
+      }
+    },
+    }
+</script>
+
+<template>
+        <span v-bind:class="classObject"
+        v-bind:style="styleObject"
+      >
+        <Icon v-if="icon" :name="icon" />
+         <span v-if="status" class-name="`avatar-status bg-${status}`" />
+        <slot/>
+        </span>
+</template>
