5bac7ac0ebe669c8caa2b355ae63dd98752d6775
Ismail bagayoko <bagayoko.ismail@gmail.com>
2019-06-02 18:40:44 -0300

    aDD JS AND CSS

--- ibaga/resources/views/blog/layouts/app.blade.php
+++ ibaga/resources/views/blog/layouts/app.blade.php
@@ -37,9 +37,13 @@
     <script defer src="https://use.fontawesome.com/releases/v5.7.1/js/all.js" integrity="sha384-eVEQC9zshBn0rFj4+TU78eNA19HMNigMviK/PU/FFjLXqa/GKPgX58rvt5Z8PLs7" crossorigin="anonymous"></script>
         <!-- Scripts -->
     <script src="https://momentjs.com/downloads/moment.js"></script>
-        {{-- <script src="{{ ('/app.js') }}" defer></script> --}}
+    <script src="{{ asset('assets/js/manifest.js') }}" defer></script>
+    <script src="{{ asset('assets/js/vendor.js') }}" defer></script>
+    <script src="{{ asset('assets/js/app.js') }}" defer></script>
         
     <!-- Additional style sheets -->
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins.css') }}" defer>
+    <link rel="stylesheet" href="{{ asset('assets/css/bundle.css') }}" defer>
     @stack('styles')
 </head>
 <body>
