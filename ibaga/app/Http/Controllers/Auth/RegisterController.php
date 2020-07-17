541133701bbe2ba99257cf9f8402bfdef5592fe1
ibagayoko <46138891+ibagayoko@users.noreply.github.com>
2020-07-16 21:17:23 -0400

    Apply fixes from StyleCI (#25)

--- ibaga/app/Http/Controllers/Auth/RegisterController.php
+++ ibaga/app/Http/Controllers/Auth/RegisterController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\User;
-use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
@@ -79,7 +79,7 @@ class RegisterController extends Controller
             'name'     => $data['name'],
             'email'    => $data['email'],
             'password' => Hash::make($data['password']),
-            ]);
+        ]);
         $user->save();
 
         return $user;
